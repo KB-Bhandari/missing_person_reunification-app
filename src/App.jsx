b99130aa5712d7bdaa8b694d2275b57cfe28bfa2
Sadiya6767 <sadiyahaldwani413@gmail.com>
2025-11-24 11:04:52 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 // src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import Login from "./pages/login";
 import VolunteerDashboard from "./pages/volunteerDashboard";
 import FamilyDashboard from "./pages/FamilyDashboard";
 import HomePage from "./pages/home.jsx";
-
 import VolunteerRegister from "./pages/volunteerRegister";
 
 import FamilyRegister from "./pages/family_Register.jsx";
 import MissingPersonInfo from "./pages/missingPersonInfo";
-
 
 const App = () => {
   return (
@@ -24,13 +22,10 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/volunteer-dashboard" element={<VolunteerDashboard />} />
           <Route path="/family-dashboard" element={<FamilyDashboard />} />
-
           {/* 🔥 Missing Routes Added */}
           <Route path="/volunteer-register" element={<VolunteerRegister />} />
           <Route path="/family-register" element={<FamilyRegister />} />
           <Route path="/missing-info" element={<MissingPersonInfo />} />
-
-
         </Routes>
       </main>
       <Footer />
